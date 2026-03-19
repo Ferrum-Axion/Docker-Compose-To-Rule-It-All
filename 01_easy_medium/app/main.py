0203ecf55a6ed11c34909faaed6b1f343c026234
Liad Binyamin <liad.binyaminn@gmail.com>
2026-03-19 21:18:56 +0200

    Inital commit

--- 01_easy_medium/app/main.py
+++ 01_easy_medium/app/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def get_db_connection():
     return psycopg2.connect(
         host=os.environ["DB_HOST"],
-        port=os.environ.get("DB_PORT", 5432),
+        port=os.environ.get("DB_PORT", 5433),
         dbname=os.environ["DB_NAME"],
         user=os.environ["DB_USER"],
         password=os.environ["DB_PASSWORD"],
